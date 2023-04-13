25a74b324bcbca9dca81211c6753a5f1435d61ff
류형석 <hyungseok@lyuhyeongseog-ui-MacBookPro.local>
2023-04-13 20:16:04 +0900

    Modify YOLO page and Add requirements.txt

--- pages/2_Webcam_with_YOLO.py
+++ pages/2_Webcam_with_YOLO.py
@@ -5,6 +5,8 @@ from torchvision import transforms
 from PIL import Image
 import numpy as np
 import cv2
+import av
+from yolov5.utils.plots import Annotator, colors
 
 class YOLOv5VideoTransformer(VideoTransformerBase):
     def __init__(self):
@@ -15,34 +17,27 @@ class YOLOv5VideoTransformer(VideoTransformerBase):
             transforms.ToTensor(),
         ])
 
-    def transform(self, frame):
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
         img = frame.to_ndarray(format="bgr24")
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         pil_img = Image.fromarray(img_rgb)
         input_img = self.preprocess(pil_img).unsqueeze(0)
-
         with torch.no_grad():
             results = self.model(input_img)
 
-        result_img = results.render()[0]
-        result_img = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
-        return result_img
+        labels = results.xyxy[0][:, -1].numpy()
+        boxes = results.xyxy[0][:, :-1].numpy()
 
-def main():
-    st.title("YOLOv5 Object Detection with Webcam")
-    st.markdown("## Webcam Video")
+        annotator = Annotator(img_rgb)
+        for i, (label, box) in enumerate(zip(labels, boxes)):
+            color = colors(int(label))
+            annotator.box_label(box, f"{label}: {box[4]:.2f}", color=color)
 
-    yolo_transformer = YOLOv5VideoTransformer()
-    
-    if "_components_callbacks" not in st.session_state:
-        st.session_state._components_callbacks = {}
-        
-    webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=yolo_transformer)
+        result_img = cv2.cvtColor(annotator.im, cv2.COLOR_RGB2BGR)
+        return av.VideoFrame.from_ndarray(result_img, format="bgr24")
 
-    if webrtc_ctx.video_transformer:
-        st.markdown("Stream is running")
-    else:
-        st.markdown("Starting stream...")
 
-if __name__ == "__main__":
-    main()
+st.header("Object Detection with YOLOv5")
+st.markdown("Click the 'Start' button below to access your webcam and see the object detection in real-time.")
+
+webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=YOLOv5VideoTransformer)
